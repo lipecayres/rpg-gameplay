20ffecc2c07c8c0337bac20bf554c80ac8b6c99c
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-07-24 20:03:31 -0300

    commit 1

---
+++ rpg/rpg.py
@@ -0,0 +1,38 @@
+# RPG Supporting System
+
+from curses.ascii import isalpha
+
+# Variaveis:
+#Nome do jogo
+game = ""  
+#Quantidade de jogadores
+playerNumber = 0   
+#Lista de players
+playerNames = []
+
+def main()
+    # introdução
+    print("Olá! Vamos jogar um jogo?")
+    
+    # Nome do jogo
+    game = input("Qual jogo estamos jogando? ")
+    print("Legal, então vamos jogar " + game)
+
+    # Personagens
+    while True:
+        playerNumber = int(input("Qual a quantidade de jogadores?"))
+        if playerNumber > 0 and playerNumber =< 6:
+            break
+        else:
+            print("No máximo 6 jogadores. Tente de novo.")
+                
+    # Nome dos personagens
+    print("Hora de definir o nome dos personagens!")
+    for player in range(playerNumber):
+        while True:
+            p = input("Qual o nome do personagem " + (player +1))
+            confirm = input("Confirma o nome: " + p + "(Sim/Nao)")
+            if confirm[0].lower() == 's':
+                playerNames.append(p)
+                break
+    
