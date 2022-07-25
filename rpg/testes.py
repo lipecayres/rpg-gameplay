fc578d541b1d8f703a5937d442a74a6aa1ddd0a3
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-07-25 10:53:35 -0300

    commit teste

--- rpg/testes.py
+++ rpg/testes.py
@@ -1,2 +1,12 @@
-u = 2
-print("o texto é " + u)
+nota_1 = float(input('Qual valor da primeira nota? '))
+nota_2 = float(input('Qual valor da segunda nota? '))
+media = (nota_1 + nota_2) / 2
+
+print(f"Resultado: {media} ", end = "")
+
+if media >= 7 and media < 10:
+    print('Aprovado')
+elif media == 10:
+    print('Aprovado com Distinção')
+else:
+    print("Reprovado") 
