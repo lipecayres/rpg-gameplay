6584e6538007c542485f5fdaae5cf1d4ae8d5e12
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-07-25 21:25:30 -0300

    Translating game

--- rpg/rpg.py
+++ rpg/rpg.py
@@ -1,81 +1,81 @@
 # RPG Supporting System
 
-# Variaveis:
+# Variables:
 
-#Nome do jogo
+#Game Name
 game = ""  
 
-#Quantidade de jogadores
+#Player's Quantity
 playerNumber = 0   
 
-#Lista de players
+#Players Username
 playerNames = []
 
-# cursor do jogo
+# Game Cursor
 cursor = 0
 
-    # game
+    # Game
 def main():
-    #inicio do jogo
+    #Starting game
     intro()
 
 
 
-    # introdução
+    # Introduction
 def intro():
     print()
-    print("Olá! Vamos jogar um jogo?")
+    print("Hi! Let's play a game?")
     print()
 
-        # Nome do jogo
-    print("Qual jogo estamos jogando? ")
-    game = input("Nome do jogo: ")
+        # Defining game name
+    print("What is the gane name we are playing? ")
+    game = input("Game name: ")
     print()
-    print("Legal, então vamos jogar " + game)
+    print("Nice! So, let's play " + game)
     print()
 
-        # Personagens
+        # Defining Players Quantity 
     while True:
-        print("Qual a quantidade de jogadores? (Max: 6)")
-        playerNumber = int(input("Quantidade: "))
+        print("How many players are playing? (Max: 6)")
+        playerNumber = int(input("Quantity: "))
         if playerNumber > 0 and playerNumber <= 6:
             break
         elif playerNumber < 0:
-            print("Pelo menos 1 jogador! Tente de novo")
+            print("At least 1 player! Try again")
         else:
-            print("No máximo 6 jogadores. Tente de novo.")
+            print("Max 6 players. Try again.")
     print()
         
-        # Nome dos personagens
-    print("Hora de definir o nome dos personagens!")
+        # Defining players username
+    print("Time to ser the player's name!")
     print()
     counter = 0
     for player in range(playerNumber):
         while True:
-            print(f"Qual o nome do personagem {counter+1}: ")
-            p = input("Digite o nome: ")
+            print(f"How should we call player {counter+1}: ")
+            p = input("Name: ")
             print()
-            print("Confirma o nome? (Sim/Nao): " + p)
-            confirm = input("Confirma?: ")
-            if confirm[0].lower() == 's':
+            print("Are you sure about the name? (Yes/No): " + p)
+            confirm = input("Confirm?: ")
+            if confirm[0].lower() == 'y':
                 playerNames.append(p)
                 counter +=1
                 break 
     print(playerNames)
 
-print("Tudo pronto, podemos iniciar o jogo!")
+print("All set! Let's start the game!")
 print()
 
 def menu():
-    print("O que você deseja fazer agora?")
-    print("1- Lançar os dados")
-    print("2- Gerenciar itens")
+    print("What do you want to do now?")
+    print("1- Roll the dice ")
+    print("2- Manage items")
     print("3- Level")
-    print("4- Ver informações")
-    print("5- Opções do jogo")
-    cursor = input("Digite a opção: ")
+    print("4- See informations")
+    print("5- Game Options")
+    cursor = input("Choose a number: ")
     if cursor == "1":
-        dados() #Pendente
+        dice() #Pendente
     if cursor == "2":
         itens() #pendente
     if cursor == "3":
@@ -83,9 +83,9 @@ def menu():
     if cursor == "4":
         info() #pendente
     if cursor == "5":
-        opcoes() #pendente
+        options() #pendente
 
-def dados():
+def dice():
     #Função para jogar os dados no jogo
     print()
 
@@ -101,7 +101,7 @@ def info():
     #função para mostrar as informações dos usuários ativos
     print()
 
-def opções():
+def options():
     #Função para abrir opções do sistema
     print() 
     
