ce4dc936d7ecad55ad33f30bf741d60d8af71c2b
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-07-29 12:48:12 -0300

    rpgGameplay v1 - OK

--- rpg/rpg.py
+++ rpg/rpg.py
@@ -1,9 +1,14 @@
-import random
-import csv
 
+##########################
 #
 # RPG Game Play
 #
+##########################
+
+#Prototypes
+import random
+import csv
+
 
 # Variables:
 
@@ -25,8 +30,9 @@ levelPlayer = []
 def main():
 
     #Starting game
-
-#    intro()  
+    
+    opening()
+    intro()  
     menu()
 
 
@@ -59,9 +65,9 @@ def menu():
     elif cursor == 3:
         menuLevel() 
     elif cursor == 4:
-        menuStatus() #pendente
+        menuStatus() 
     elif cursor == 5:
-        menuOptions() #pendente
+        menuOptions() #pending
     else:
         print()
         print("Invalid entry. Try again")
@@ -69,115 +75,23 @@ def menu():
         menu()
 
 
-
-def menuOptions():
-    #Função para abrir opções do sistema  
-
-
-    print("//////////////////////////")
-    print(" ~~ Game Options")
+def opening():
+    
+    print("################################")
+    print("#")
+    print("# RPG Game Play")
+    print("#")
+    print("# This game play allows you") 
+    print("# to keep in track")
+    print("# in your RPG Game!") 
+    print("#")
+    print("# Have fun!")
+    print("#")
+    print('# Developed by: Felipe Cayres')
+    print("################################")
     print()
-    print("1- Save Game ")
-    print("2- Load Game")
-    print("3- Reset Game")
-    print("4- Return")
-    print("//////////////////////////")
-    print()
 
-    cursor = -1
-    while True:
-        cursor = input("Choose a number: ")
-        if cursor.isdigit():
-            cursor = int(cursor)
-            break
-        else:
-            print()
-            print("Insert a number!")
-            print()
- 
-    if cursor == 1:
-        print()
-        saveGame() #pendente 
 
-    elif cursor == 2:
-        print()
-        loadGame() #pendente 
-
-    elif cursor == 3:
-        print()
-        resetGame() #pendente
-       
-    elif cursor == 4:
-        print()
-        menu()
-
-    else:
-        print()
-        print("Invalid entry. Try again")
-        print()        
-        menuOptions()
-
-def saveGame():
-
-    print("ATTENTION! Same name -> Overwrite!")
-    savefile = input("FILE NAME: ")
-    with open(savefile + ".csv", "w") as file:
-        writer = csv.writer(file)
-        writer.writerow([game,playerNumber, playerNames, inventory, levelPlayer])
-
-    print("Game saved as:  " + savefile)
-    menuOptions()
-
-def loadGame():
-   
-    global load    
-    load = []
-
-    print("Let's load your game!")
-    loading = input("FILE NAME: ")     
-    with open(loading + ".csv", "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            load.append(row)
-
-    global game 
-    global playerNumber
-    global playerNames
-    global inventory 
-    global levelPlayer
-
-    game = load[0][0]
-    playerNumber = load[0][1]
-    playerNames = load[0][2]
-    inventory = load[0][3]
-    levelPlayer = load[0][4]
-
-    print()
-    print("###########################")
-    print("GAME " + loading + " LOADED!")
-    print("###########################")
-    print()
-    menu()
-
-def resetGame():
-    print("Are you sure about it?")
-    print("All your progress will be loss")    
-    print()
-    confirm = input("Confirm? (Yes/No): ")
-    if confirm[0].lower() == 'y':
-        game = ""  
-        playerNumber = 0   
-        playerNames = []
-        inventory = []
-        levelPlayer = []
-
-        main()
-
-    else:
-        print()
-        print("Invalid entry. Try again!")
-        print()
-        menuOptions()
 
 
 ##################################################
@@ -736,6 +650,143 @@ def menuStatus():
     print()
     print()
     menu()
+
+
+##################################################
+##################################################
+##################################################
+
+
+####### HAVE TO FIX!!! - MENU STATUS PLAYER  ##############
+
+
+def menuOptions():
+    #Função para abrir opções do sistema  
+
+
+    print("//////////////////////////")
+    print(" ~~ Game Options")
+    print()
+    print("1- Save Game ")
+    print("2- Load Game")
+    print("3- Reset Game")
+    print("4- Return")
+    print("//////////////////////////")
+    print()
+
+    cursor = -1
+    while True:
+        cursor = input("Choose a number: ")
+        if cursor.isdigit():
+            cursor = int(cursor)
+            break
+        else:
+            print()
+            print("Insert a number!")
+            print()
+ 
+    if cursor == 1:
+        print()
+        saveGame() #pendente 
+
+    elif cursor == 2:
+        print()
+        loadGame() #pendente 
+
+    elif cursor == 3:
+        print()
+        resetGame() #pendente
+       
+    elif cursor == 4:
+        print()
+        menu()
+
+    else:
+        print()
+        print("Invalid entry. Try again")
+        print()        
+        menuOptions()
+
+def saveGame():
+
+    print(" WOrking on it...")
+
+    menuOptions()
+
+    print("ATTENTION! Same name -> Overwrite!")
+    savefile = input("FILE NAME: ")
+    with open(savefile + ".csv", "w", encoding = "UTF8", newline = "\n") as file:
+        writer = csv.writer(file)
+        writer.writerow([game])
+        writer.writerow([playerNumber])
+        writer.writerow([playerNames])
+        writer.writerow([inventory])
+        writer.writerow([levelPlayer])
+
+    print("Game saved as:  " + savefile)
+    menuOptions()
+
+def loadGame():
+    
+    print(" WOrking on it...")
+
+    menuOptions()
+
+    global load    
+    load = []
+
+    print("Let's load your game!")
+    loading = input("FILE NAME: ")     
+    with open(loading + ".csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            load.append(row)
+
+    global game 
+    global playerNumber
+    global playerNames
+    global inventory 
+    global levelPlayer
+
+    game = load[0][0]
+    playerNumber = load[0][1]
+    playerNames = load[0][2]
+    inventory = load[0][3]
+    levelPlayer = load[0][4]
+
+    print()
+    print("###########################")
+    print("GAME " + loading + " LOADED!")
+    print("###########################")
+    print()
+    menu()
+
+def resetGame():
+    
+    global game, playerNumber, playerNames, inventory, levelPlayer
+    
+    print("Are you sure about it?")
+    print("All your progress will be loss")    
+    print()
+    confirm = input("Confirm? (Yes/No): ")
+    if confirm[0].lower() == 'y':
+        game = ""  
+        playerNumber = 0   
+        playerNames = []
+        inventory = []
+        levelPlayer = []
+       
+        intro()
+
+    else:
+        print()
+        print("Invalid entry. Try again!")
+        print()
+        menuOptions()
+
+
+
+
 
 
 
