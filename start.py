d7a315a604efe57f1dd804011a327441a4327a33
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-28 18:27:17 -0300

    Starting Game

---
+++ start.py
@@ -0,0 +1,26 @@
+
+##########################
+#
+# RPG Game Play
+#
+##########################
+
+#Prototypes
+
+import intro
+
+
+#
+### MAIN GAME 
+#
+
+def start():
+
+    #Starting game
+    
+    intro.opening()
+    intro.intro()  
+    intro.menu()
+
+
+start()
