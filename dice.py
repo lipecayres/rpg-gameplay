cb21b61c3e81d9d97688c2419e19d5e5a8564b83
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-28 18:28:05 -0300

    function | move to specific file - dice

---
+++ dice.py
@@ -0,0 +1,56 @@
+import random
+import intro
+
+
+############ READY - MENU DICE ###################
+
+def menuDice():
+    print()
+    print("--------------------------------")
+    print("How many dices do we play?")
+    print("1- One dice")
+    print("2- Two dices")
+    print("3- Return")
+    print("--------------------------------")
+
+    while True:
+        cursor = input("Choose a number: ")
+        if cursor.isdigit():
+            cursor = int(cursor)
+            break
+        else:
+            print()
+            print("Insert a number!")
+            print()
+ 
+    if cursor == 1:
+        sortingDice1() 
+    elif cursor == 2:
+        sortingDice2()
+    elif cursor == 3:
+        intro.menu() 
+    else:
+        print("Invalid entry. Try again")
+        menuDice()
+
+def sortingDice1():
+    sortDice = random.randint(1,6) 
+    print()
+    print("..........Sorting dice..........")
+    print()
+    print("Here's the sorted number:")
+    print(f"Dice: {sortDice}")
+    
+    menuDice()
+    
+def sortingDice2():
+    sortDice1 = random.randint(1,6) 
+    sortDice2 = random.randint(1,6)
+    print()
+    print("..........Sorting dices..........")
+    print()
+    print("Here's the sorted number:")
+    print(f"Dice 1: {sortDice1}")
+    print(f"Dice 2: {sortDice2}")
+
+    menuDice()
